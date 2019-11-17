d22eeb720443ab8aa106a66c0015b07d2e1c9e3c
FelipeTrebino <fliphd1505@gmail.com>
2019-11-17 18:21:02 -0300

    Initial commit

--- Site 2.0/visao/criartopico.php
+++ Site 2.0/visao/criartopico.php
@@ -1,4 +1,4 @@
-<div id="stickyadcontainer" style="width: 300px; position: fixed; top: 900px; left: 1750px;">
+<div id="stickyadcontainer" style="width: 300px; position: fixed; top: 85%; left: 90%;">
     <div style="position:relative;margin:auto;">
       	<form action="controle/criarTopico.php">
       		<button type="submit" class="btn" style="background-color: #ff8533; border-style: solid; border-color: #777777; color: white; font-family: arial;">Criar Topico <b>+</b>
